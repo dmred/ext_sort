822d9e8706f99481318582a2cb6473fd519222b6
dmred <dmredchenko@gmail.com>
2016-11-28 04:49:21 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -28,7 +28,7 @@ SCENARIO("ext1", "[in100]")
 
 SCENARIO("ext2", "[in20k]")
 {
-  sorting("in100.txt", "output20000k.txt", 4);
+  sorting("in100.txt", "output20000k.txt", 64);
 	string *str1 = new string[20000], *str2 = new string[20000];
 	bool flag = false;
 	ifstream f1("output20000k.txt"), f2("test20000k.txt");
