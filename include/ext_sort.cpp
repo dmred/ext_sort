fe80c2cb9bca8503a3022d4e2ed9300ccc84f4f5
dmred <dmredchenko@gmail.com>
2016-11-28 04:22:25 +0300

    Update ext_sort.cpp

--- include/ext_sort.cpp
+++ include/ext_sort.cpp
@@ -23,25 +23,22 @@ struct inp
 
 auto sorting(const std::string input_adress, const std::string output_adress, const unsigned int memory)
 {
-	std::ifstream fin("C:/Users/Dell/Documents/Visual Studio 2015/Projects/extended_sort/input700000k.txt");
-	//std::ifstream fin(input_adress);
+	//std::ifstream fin("C:/Users/Dell/Documents/Visual Studio 2015/Projects/extended_sort/input700000k.txt");
+	std::ifstream fin(input_adress);
 	if (fin.is_open() == false) throw ("Cant open ur file");
-	std::ofstream fout("C:/Users/Dell/Documents/Visual Studio 2015/Projects/extended_sort/output700000k.txt");
-	//std::ofstream fout(output_adress);
+	//std::ofstream fout("C:/Users/Dell/Documents/Visual Studio 2015/Projects/extended_sort/output700000k.txt");
+	std::ofstream fout(output_adress);
 	size_t num_buff = 0;
 	while (fin.eof() == false)
 	{
 		std::string s;
 		std::ofstream buff(std::to_string(num_buff+1) + ".txt");
-		//std::vector<std::string> vector_;
 		std::deque<std::string> deque_;
-		for (unsigned int size = 0; (size + sizeof(std::string)) <= memory; size += sizeof(std::string))
+		for (unsigned int size = 0; (size + sizeof(std::string)) < memory*1024*1024; size += sizeof(std::string))
 		{
 			std::getline(fin, s);
-			//vector_.push_back(s);
 			deque_.push_back(s);
 		}
-		//std::sort(vector_.begin(), vector_.end());
 		std::sort(deque_.begin(), deque_.end());
 		for (auto i : deque_)
 		{
@@ -92,7 +89,6 @@ auto sorting(const std::string input_adress, const std::string output_adress, co
 // 	std::cout << "input path: "; std::cin >> in;
 // 	std::cout << "output path: "; std::cin >> out;
 // 	std::cout << "write memory(mb): "; std::cin >> memory_size;
-// 	memory_size *= 1024;
 // 	std::chrono::time_point<std::chrono::system_clock> start, end;
 // 	start = std::chrono::system_clock::now();
 // 	sorting(in, out, memory_size);
