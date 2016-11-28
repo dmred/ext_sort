9634712e19dc1e7150855e5d230220b98cb4b1b6
dmred <dmredchenko@gmail.com>
2016-11-28 11:06:35 +0300

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -1,48 +1,60 @@
-#include <ext_sort.cpp>
+#include "ext_sort.cpp"
 #include <catch.hpp>
-#include <string>
 #include <iostream>
+#include <string>
 #include <fstream>
-
+#include <locale> 
 using namespace std;
+ 
+SCENARIO("8mb", "[8mb]"){
+ setlocale(LC_ALL, "Russian");
+ sorting("8.txt","8out.txt",1);
+ ifstream hay("8out.txt");
+ifstream file("test8.txt");
 
-SCENARIO("ext1", "[in100]")
-{
-  	sorting("in100.txt", "out100.txt", 0.000002);
-	string *str1 = new string[100], *str2 = new string[100];
-	bool flag = false;
-	ifstream f1("out100.txt"), f2("test100.txt");
-	for (int i = 0; i < 100; ++i)
-	{
-		getline(f1, str1[i]);
-		getline(f2, str2[i]);
-	}
-	size_t st = 0;
-	for (int i = 0; i < 100; ++i)
-	{
-		if (str1[i] == str2[i]) ++st;
-	}
-	if (st == 100) flag = true;
-  REQUIRE(flag);
-} 
-/*
-SCENARIO("ext2", "[in20k]")
-{
-  sorting("in100.txt", "output20000k.txt", 16);
-	string *str1 = new string[20000], *str2 = new string[20000];
-	bool flag = false;
-	ifstream f1("output20000k.txt"), f2("test20000k.txt");
-	for (int i = 0; i < 4000; ++i)
-	{
-		getline(f1, str1[i]);
-		getline(f2, str2[i]);
-	}
-	size_t st = 0;
-	for (int i = 0; i < 4000; ++i)
-	{
-		if (str1[i] == str2[i]) ++st;
-	}
-	if (st == 4000) flag = true;
-  REQUIRE(flag);
-} 
-*/
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+getline(file,s1);
+ getline(hay,s2);
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
+  }
+ }
+  file.close();
+ hay.close();
+  REQUIRE(p==true);
+}
+
+SCENARIO("15mb", "[15mb]"){
+ setlocale(LC_ALL, "Russian");
+ sorting("15.txt","15out.txt",4);
+ ifstream hay("15out.txt");
+ifstream file("test15.txt");
+
+int i=0;
+ bool p=true;
+ string s1,s2;
+ while (!hay.eof()&&!file.eof()){
+getline(file,s1);
+ getline(hay,s2);
+  i++;
+  if (s1!=s2){
+  p=false;
+   cout<<i<<endl;
+   cout<<s1<<endl;
+   cout<<s2<<endl;
+   break;
+  }
+ }
+  file.close();
+ hay.close();
+  REQUIRE(p==true);
+}
+
