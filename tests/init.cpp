322f1c64593c39a76da6fa490890999ad1a138fe
dmred <dmredchenko@gmail.com>
2016-12-12 18:28:57 +0400

    Update init.cpp

--- tests/init.cpp
+++ tests/init.cpp
@@ -8,7 +8,14 @@ using namespace std;
  
 SCENARIO("8mb", "[8mb]"){
  setlocale(LC_ALL, "Russian");
+ std::chrono::time_point<std::chrono::system_clock> start, end;
+start = std::chrono::system_clock::now();
  sorting("8.txt","8out_name.txt",1);
+ end = std::chrono::system_clock::now();
+int elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>
+(end - start).count();
+std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+std::cout << "finished computation at " << std::ctime(&end_time) << "elapsed time: " << elapsed_seconds << "s\n";
  ifstream file("test8_name.txt");
 ifstream hay("test8_name.txt");
 int i=0;
